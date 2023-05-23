98262b76a7125475395a86cc6acb2a7975c70cff
EPONCHINTSEV DMITRII <deponchintsevv@gmail.com>
2023-05-23 11:51:39 +0545

    fix mediagroup sending trouble

--- bot.py
+++ bot.py
@@ -1,16 +1,32 @@
+import json
 from aiogram import Bot, Dispatcher, types
-import asyncio
 from config import BOT_TOKEN
+from database import add_entry, update_entry_status
+
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
-def marks_keyboard():
+def marks_keyboard(data: str):
+    yes = f"{data}:1"
+    no = f"{data}:0"
+    print(yes)
     keyboard = types.InlineKeyboardMarkup(row_width=2)
-    button_like = types.InlineKeyboardButton(text='Нравится', callback_data='like')
-    button_dislike = types.InlineKeyboardButton(text='Не нравится', callback_data='dislike')
+    button_like = types.InlineKeyboardButton(text='Нравится', callback_data=yes)
+    button_dislike = types.InlineKeyboardButton(text='Не нравится', callback_data=no)
     keyboard.add(button_like, button_dislike)
     return keyboard
+
+
+@dp.callback_query_handler()
+async def button1_handler(callback_query: types.CallbackQuery):
+    message_id = callback_query.message.message_id
+    chat_id = callback_query.message.chat.id
+    data = callback_query.data.split(":")
+    post_id = int(data[0])
+    mark = int(data[-1])
+    update_entry_status(post_id, mark)
+    await bot.delete_message(chat_id=chat_id, message_id=message_id)
 
 
 post = {"files": []}
@@ -53,9 +69,11 @@ async def forward_message(message: types.Message):
                                 media_group.attach_video(file[-1])
                             elif file[0] == "document":
                                 media_group.attach_document(file[-1])
+                    #сохраняю текст в бд номне нужен id записи.
+                    identificator = add_entry(user_id=735551549, text=post["message"])
                     await bot.send_media_group(chat_id=735551549, media=media_group)
                     await bot.send_message(chat_id=735551549, text="Понравился ли вам пост?",
-                                           reply_markup=marks_keyboard())
+                                           reply_markup=marks_keyboard(data = f"{identificator}:735551549"))
                     post = {"files": []}
             else:
                 post["counter"] = message.caption[-1]
